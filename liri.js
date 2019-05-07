ac0ae5e3648cec9a13630960f0b466ad0a3ddb61
unknown <tonycruz417@gmail.com>
2019-05-07 09:57:20 -0500

    added new parameters

--- liri.js
+++ liri.js
@@ -9,13 +9,23 @@ var spotify = new Spotify({
 
 var keys = require("./keys.js")
 var axios = require("axios");
+var songName = process.argv[2];
 
-spotify.request('https://api.spotify.com/v1/search?q=Tap&type=track%2Cartist&market=US&limit=10&offset=5')
+// spotify.request('https://api.spotify.com/v1/search?q=' + songName +'&type=track%2Cartist&market=US&limit=10&offset=5')
+// .then(function(data) {
+//     var newData = JSON.stringify(data);
+//     console.log(newData);
+// })
+
+spotify.request('https://api.spotify.com/v1/search?q=' + songName + '&type=track&limit=2')
 .then(function(data) {
-    var newData = JSON.stringify(data);
-    console.log(newData);
+    // var newData = JSON.stringify(data);
+    // console.log(data.tracks.items);
+    console.log(data.tracks.items.album);
 })
 
-if(process.argv[2] === "spotify") {
-    console.log("hey it worked");
-}
+
+
+// if(process.argv[2] === "spotify") {
+//     console.log("hey it worked");
+// }
