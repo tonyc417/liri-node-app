ee3c9c8abb81d5a1d4a55b0734b7a7598534eff4
Tony Cruz <tonycruz@Tonys-MacBook-Pro.local>
2019-05-08 18:27:27 -0500

    added new spotify call

--- liri.js
+++ liri.js
@@ -9,23 +9,15 @@ var spotify = new Spotify({
 
 var keys = require("./keys.js")
 var axios = require("axios");
-var songName = process.argv[2];
+var songName = process.argv[3];
 
-// spotify.request('https://api.spotify.com/v1/search?q=' + songName +'&type=track%2Cartist&market=US&limit=10&offset=5')
-// .then(function(data) {
-//     var newData = JSON.stringify(data);
-//     console.log(newData);
-// })
-
-spotify.request('https://api.spotify.com/v1/search?q=' + songName + '&type=track&limit=2')
-.then(function(data) {
-    // var newData = JSON.stringify(data);
-    // console.log(data.tracks.items);
-    console.log(data.tracks.items.album);
+if(process.argv[2] === "spotify") {
+  spotify.request('https://api.spotify.com/v1/search?q=' + songName + '&type=track&limit=2')
+  .then(function(data) {
+    console.log("You searched for: " + songName);
+    console.log("Artist: " + data.tracks.items[0].artists[0].name);
+    console.log("Album Name: " + data.tracks.items[0].album.name);
+    console.log("Song Name: " + data.tracks.items[0].name);
+    console.log("Preview Link: " + data.tracks.items[0].external_urls.spotify);
 })
-
-
-
-// if(process.argv[2] === "spotify") {
-//     console.log("hey it worked");
-// }
+}
