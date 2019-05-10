792b9d244858b228f7df747ba32026878d983acb
Tony Cruz <tonycruz@Tonys-MBP.frontierlocal.net>
2019-05-10 18:46:02 -0500

    added omdb api

--- liri.js
+++ liri.js
@@ -1,18 +1,22 @@
 require("dotenv").config();
 
 var Spotify = require('node-spotify-api');
+var keys = require("./keys.js")
+var axios = require("axios");
 
 var spotify = new Spotify({
     id: process.env.SPOTIFY_ID,
     secret: process.env.SPOTIFY_SECRET
   });
 
-var keys = require("./keys.js")
-var axios = require("axios");
-var songName = process.argv[3];
+console.log(process.argv);
 
-if(process.argv[2] === "spotify") {
-  spotify.request('https://api.spotify.com/v1/search?q=' + songName + '&type=track&limit=2')
+var userInput = process.argv[3];
+
+var queryUrl = "http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy";
+
+if (process.argv[2] === "spotify") {
+  spotify.request('https://api.spotify.com/v1/search?q=' + userInput + '&type=track&limit=2')
   .then(function(data) {
     console.log("You searched for: " + songName);
     console.log("Artist: " + data.tracks.items[0].artists[0].name);
@@ -20,4 +24,12 @@ if(process.argv[2] === "spotify") {
     console.log("Song Name: " + data.tracks.items[0].name);
     console.log("Preview Link: " + data.tracks.items[0].external_urls.spotify);
 })
+}
+
+if (process.argv[2] === "movie-this" || process.argv[2] === "movie this") {
+  axios.get(queryUrl).then(
+    function(response) {
+      console.log(response);
+    }
+  )
 }
